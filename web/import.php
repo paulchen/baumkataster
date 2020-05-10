c8712dfe4719d11963594ba1dabf5594a3ea9038
paulchen <paulchen@rueckgr.at>
2020-05-10 23:45:37 +0200

    download data from data.wien.gv.at

--- web/import.php
+++ web/import.php
@@ -7,9 +7,20 @@ require_once('Csv.class.php');
 
 $columns = 'FID,OBJECTID,SHAPE,BAUM_ID,DATENFUEHRUNG,BEZIRK,OBJEKT_STRASSE,GEBIETSGRUPPE,GATTUNG_ART,PFLANZJAHR,PFLANZJAHR_TXT,STAMMUMFANG,STAMMUMFANG_TXT,BAUMHOEHE,BAUMHOEHE_TXT,KRONENDURCHMESSER,KRONENDURCHMESSER_TXT,BAUMNUMMER,SE_ANNO_CAD_DATA,lon,lat';
 
+echo("Downloading data\n");
+$filename = tempnam('/tmp', 'baumkataster_');
+$data = file_get_contents('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:BAUMKATOGD&srsName=EPSG:4326&outputFormat=csv');
+if(!$data) {
+	echo("Error downloading data\n");
+	die(1);
+}
+file_put_contents($filename, $data);
+unset($data);
+
+echo("Parsing data\n");
+
 $csv = new Csv();
-$csv->parse('BAUMKATOGD.csv');
-#$csv->parse('test.csv');
+$csv->parse($filename);
 
 $columns_array = explode(',', $columns);
 $placeholders = preg_replace('/[^,]+/', '?', $columns);
@@ -17,11 +28,10 @@ $placeholders = preg_replace('/[^,]+/', '?', $columns);
 $query = "INSERT INTO baumkataster ($columns) VALUES ($placeholders) ON DUPLICATE KEY UPDATE ";
 $query .= implode(', ', array_map(function($column) { return "$column = ?"; }, explode(',', $columns)));
 
+echo("Importing data\n");
+
 $db->beginTransaction();
-$index = 0;
 foreach($csv->rows as $row) {
-	$index++;
-	print("$index\n");
 	if($row[0] == 'FID') {
 		continue;
 	}
@@ -42,4 +52,8 @@ foreach($csv->rows as $row) {
 	db_query($query, $row);
 }
 $db->commit();
+
+unlink($filename);
+
+echo("Done\n");
 
