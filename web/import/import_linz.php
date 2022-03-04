03a9b5048d06e9079bc81773e93a2eca831086fa
paulchen <paulchen@rueckgr.at>
2022-03-04 17:28:31 +0100

    download metadata via data.gv.at's API to retrieve the correct download URL for the actual data

--- web/import/import_linz.php
+++ web/import/import_linz.php
@@ -19,13 +19,34 @@ function create_name($gattung, $art, $sorte, $name_deutsch) {
 	return $text;
 }
 
+function get_data_url($id) {
+	$url = "https://www.data.gv.at/katalog/api/3/action/package_show?id=$id";
+
+	$data = @file_get_contents($url);
+	$json = @json_decode($data, true);
+
+	if ($data === false || $json == null || !isset($json['result']) || !isset($json['result']['resources']) || !is_array($json['result']['resources']) 
+			|| count($json['result']['resources']) == 0 || !isset($json['result']['resources'][0]['url'])) {
+		return false;
+	}
+	return $json['result']['resources'][0]['url'];
+}
+
 $columns = 'BAUM_ID,GATTUNG_ART,STAMMUMFANG,STAMMUMFANG_TXT,BAUMHOEHE,BAUMHOEHE_TXT,KRONENDURCHMESSER,KRONENDURCHMESSER_TXT,BAUMNUMMER,lon,lat,source,outdated';
 
-log_info("[Linz] Downloading data");
-$data = file_get_contents('https://data.linz.gv.at/katalog/umwelt/baumkataster/2022/FME_BaumdatenBearbeitet_OGD_20220202.csv');
+log_info("[Linz] Downloading metadata");
+$url = get_data_url('f660cf3f-afa9-4816-aafb-0098a36ca57d');
+if($url === false) {
+	$data = false;
+	log_info("[Linz] Error downloading metadata");
+}
+else {
+	log_info("[Linz] Downloading data from URL: $url");
+	$data = file_get_contents($url);
+}
 if($data === false) {
 	$error = 1;
-	log_info("[Linz] - Error downloading data");
+	log_info("[Linz] Error downloading data");
 }
 else {
 	$data = iconv('ISO-8859-1', 'UTF-8', $data);
